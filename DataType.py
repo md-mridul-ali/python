a4b265bad308205711a1c67e1101dae7fe9075fd
MD.Mridul Ali <136635472+md-mridul-ali@users.noreply.github.com>
2025-03-17 00:14:31 +0600

    Data Type

---
+++ DataType.py
@@ -0,0 +1,20 @@
+var_1=123
+var_2=20.23
+print(var_1 + var_2)
+print(type(var_1))
+print(type(var_2))
+var_3=0b10
+var_4=0o10
+var_5=0x10
+print(var_3)
+print(var_4)
+print(var_5)
+print(type(var_3))
+print(type(var_4))
+print(type(var_5))
+name="Sadia"
+print(type(name))
+print(name[2])
+#Out put should be Jenny's "lecture"
+print("Jenny\'s \"lecture\" ")
+print(5*"I love my motherland\n")
