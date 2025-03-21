33d4bb51a5c136ba5436db64dde4573ea85202fc
MD.Mridul Ali <136635472+md-mridul-ali@users.noreply.github.com>
2025-03-21 21:07:04 +0600

    BMI

---
+++ calculate_bmi.py
@@ -0,0 +1,5 @@
+weight=input("Enter your weight in kg: ")
+height=input("Enter your height in meter: ")
+bmi=int(weight)/float(height)**2
+print("Your bmi is:",bmi)
+print("Your bmi is in integer formate:",int(bmi))
